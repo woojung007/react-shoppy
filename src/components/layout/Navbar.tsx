187f463dd44c6e51c9cf14f37abbc4a31c135f0f
woojung007 <eeeeve007@gmail.com>
2023-07-23 13:50:13 +0900

    feat: navbar css

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaShopify } from 'react-icons/fa';
-import { HiPencil } from 'react-icons/hi';
+import { HiPencil, HiShoppingCart } from 'react-icons/hi';
+import { RiAccountCircleFill } from 'react-icons/ri';
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to='/'>
+    <header className='flex justify-between border-b border-gray-300 p-2'>
+      <Link to='/' className='flex items-center text-4xl text-brand'>
         <FaShopify />
         <h1>Shoppy</h1>
       </Link>
 
-      <nav>
+      <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link to='/products'>Products</Link>
-        <Link to='/products/new'>
+        <Link to='/carts' className='text-2xl'>
+          <HiShoppingCart />
+        </Link>
+        <Link to='/products/new' className='text-2xl'>
           <HiPencil />
         </Link>
 
-        <button>Login</button>
+        <button className='text-2xl'>
+          <RiAccountCircleFill />
+        </button>
       </nav>
     </header>
   );
