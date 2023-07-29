3c2b27e60b52d59a5323947316a9888d5aaa2630
woojung007 <eeeeve007@gmail.com>
2023-07-29 17:27:00 +0900

    feat: user profile style 추가

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -9,9 +9,13 @@ export default function UserProfile({
   user: { photoURL, displayName },
 }: UserProps) {
   return (
-    <div>
-      <img src={photoURL!} alt={displayName!} />
-      <span>{displayName}</span>
+    <div className='flex items-center'>
+      <img
+        className='w-10 h-10 rounded-full mr-2'
+        src={photoURL!}
+        alt={displayName!}
+      />
+      <span className='hidden md:block'>{displayName}</span>
     </div>
   );
 }
